f3aef7f7cd54690b8ef206a671db529d01d0d565
Ma <asasf@sfsfsdf.com>
2022-11-25 16:24:58 +0200

    Toteutettu testi Yhden tuotteen lisäämisen jälkeen ostoskorissa on 1 tavara, sekä tarvittava toiminnallisuus.

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,24 +3,24 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        pass
+        #pass
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote        
-        #self.kori = []
+        self.kori = []
     
     def tavaroita_korissa(self):
         #pass
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
-        """
-        int tavaroita = 0
+        
+        tavaroita = 0
 
         for ostos in self.kori:
             tavaroita += ostos.lukumaara()
         
         return tavaroita
-        """
-        return 0
+        
+        #return 0
 
     def hinta(self):
         return 0
@@ -28,7 +28,14 @@ class Ostoskori:
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        pass
+        #pass
+        for ostos in self.kori:
+            if ostos.tuotteen_nimi() == lisattava.nimi():
+                ostos.muuta_lukumaaraa(1)
+                return
+
+        ostos = Ostos(lisattava)
+        self.kori.append(ostos)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
