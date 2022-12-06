359af142be95c84b0b78e9319df78aacf225c864
Ma <asasf@sfsfsdf.com>
2022-12-06 22:47:00 +0200

    commit

--- viikko5/int-joukko/src/index.py
+++ viikko5/int-joukko/src/index.py
@@ -1,9 +1,7 @@
 import unittest
 from int_joukko import IntJoukko
 
-
 def main():
-    """
     joukko = IntJoukko()
 
     joukko.lisaa(1)
@@ -16,49 +14,6 @@ def main():
     joukko.poista(2)
 
     print(joukko.to_int_list())
-    """
-
-    lisattavat = [1, 2, 4, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-
-
-    
-    joukko_a = IntJoukko()
-    joukko_b = IntJoukko(8)
-    joukko_c = IntJoukko(10, 20)
-
-    print(joukko_a.to_int_list())
-    print()
-    print(joukko_b.to_int_list())
-    print()
-    print(joukko_c.to_int_list())
-    print()
-    print()
-
-    lisaa(joukko_a)
-    lisaa(joukko_b)
-    lisaa(joukko_b)
-
-    print(joukko_a.to_int_list())
-    print()
-    print(joukko_b.to_int_list())
-    print()
-    print(joukko_c.to_int_list())
-    print()
-    print()
-    """
-    self.toimii_kasvatuksen_jalkeen(joukko_a)
-    self.toimii_kasvatuksen_jalkeen(joukko_b)
-    self.toimii_kasvatuksen_jalkeen(joukko_c)
-    """
-
-def lisaa(joukko):
-    lisattavat = [1, 2, 4, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-
-    for luku in lisattavat:
-        print(luku)
-        joukko.lisaa(luku)
-        
-
 
 if __name__ == "__main__":
     main()
